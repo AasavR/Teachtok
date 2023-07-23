28e9afaa8ad8773005efd9b060def5f289ae4a11
aasavravi <aasavravi@gmail.com>
2023-07-23 19:11:50 +0530

    Create AppNavigator.tsx

---
+++ AppNavigator.tsx
@@ -0,0 +1,33 @@
+// AppNavigator.tsx
+
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import DiscoverScreen from './screens/DiscoverScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen 
+        name="Home" 
+        component={HomeScreen} 
+        options={{headerShown: false}}
+      />
+      <Stack.Screen 
+        name="Profile"
+        component={ProfileScreen}
+      />
+      <Stack.Screen 
+        name="Discover" 
+        component={DiscoverScreen} 
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default AppNavigator;
